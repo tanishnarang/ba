75d7a7d41aeaed7e3a176b73b39cd5a48327efa1
Tanish Narang <narangtanish2@gmail.com>
2024-04-17 15:01:22 +0530

    implemented nav bar button functions

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ button.addEventListener("click", function (e) {
   const gotos = section.getBoundingClientRect();
   section.scrollIntoView({ behavior: "smooth" });
 });
+
+// smooth functioning of nav bar buttons.
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
